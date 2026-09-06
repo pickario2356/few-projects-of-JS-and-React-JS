1270bbd791ed1096f885a3add4aba1d69c70fbe8
Priyanshu <pickario2356@gmail.com>
2026-09-06 22:27:01 +0530

    revised the core topics of react

--- REACT/K72-clone (GSAP)/src/App.jsx
+++ REACT/K72-clone (GSAP)/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
+import { Route, Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/agence' element={<Agence />} />
+        <Route path='/projects' element={Projects} /> 
+      </Routes>
+    </div>
   )
 }
 
