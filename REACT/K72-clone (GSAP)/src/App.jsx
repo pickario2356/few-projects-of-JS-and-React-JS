7acfa7983679c5ed399c662add064dbad7295068
Priyanshu <pickario2356@gmail.com>
2026-09-07 22:50:19 +0530

    revised few react topics and learnt some shortcuts

--- REACT/K72-clone (GSAP)/src/App.jsx
+++ REACT/K72-clone (GSAP)/src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Home from './pages/Home'
-import Agence from './pages/Agence'
-import Projects from './pages/Projects'
-import { Route, Routes } from 'react-router-dom'
-
+import React from "react";
+import Home from "./pages/Home";
+import Agence from "./pages/Agence";
+import Projects from "./pages/Projects";
+import { Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
+      <Link to="/">Home</Link>
+      <Link to="/agence">Agence</Link>
+      <Link to="/projects">Projects</Link>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/agence' element={<Agence />} />
-        <Route path='/projects' element={Projects} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/agence" element={<Agence />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
