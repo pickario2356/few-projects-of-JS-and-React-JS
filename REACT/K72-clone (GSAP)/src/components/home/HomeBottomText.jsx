12c7989b4bf790518914837deb28a114a6d5877f
Priyanshu <pickario2356@gmail.com>
2026-09-10 17:29:11 +0530

    made the homepage

--- REACT/K72-clone (GSAP)/src/components/home/HomeBottomText.jsx
+++ REACT/K72-clone (GSAP)/src/components/home/HomeBottomText.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeBottomText = () => {
   return (
-    <div>
-        <Link>Projects</Link>
-        <Link>Agence</Link>
+    <div className="font-[font-2] gap-2 flex items-center justify-center">
+      <Link
+        className="text-[7.5vh] leading-[6vh] border-5 pt-3 rounded-full uppercase px-10 "
+        to="/projects"
+      >
+        Projects
+      </Link>
+      <Link
+        className="text-[7.5vh] leading-[6vh]  border-5 pt-3 rounded-full uppercase px-10 "
+        to="/agence"
+      >
+        Agence
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBottomText
+export default HomeBottomText;
--- REACT/K72-clone (GSAP)/src/components/home/HomeHeroText.jsx
+++ REACT/K72-clone (GSAP)/src/components/home/HomeHeroText.jsx
@@ -4,17 +4,17 @@ import Video from "./Video";
 const HomeHeroText = () => {
   return (
     <div className="font-[font1] text-center">
-      <div className="uppercase text-[9.5vh] flex items-center justify-center  leading-[9.5vh] ">
+      <div className="uppercase text-[9.5vw] flex items-center justify-center  leading-[9.5vw] ">
         The spark for
       </div>
-      <div className="uppercase text-[9.5vh] flex items-center justify-center leading-[9.5vh] ">
+      <div className="uppercase text-[9.5vw] flex items-center justify-center leading-[9.5vw] ">
         all
-        <div className="h-[8vh] rounded-full overflow-hidden">
+        <div className="h-[7vw] rounded-full -mt-3 overflow-hidden">
           <Video />
         </div>
         things
       </div>
-      <div className="uppercase text-[9.5vh] flex items-center justify-center  leading-[9.5vh] ">
+      <div className="uppercase text-[9.5vw] flex items-center justify-center  leading-[9.5vw] ">
         creative
       </div>
     </div>
