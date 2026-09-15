773cd7274c6bb4e21876608933d2dad9c3290fcd
Priyanshu <pickario2356@gmail.com>
2026-09-15 22:14:22 +0530

    fixed some bugs

--- REACT/K72-clone (GSAP)/src/components/home/HomeBottomText.jsx
+++ REACT/K72-clone (GSAP)/src/components/home/HomeBottomText.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const HomeBottomText = () => {
   return (
-    <div className="font-[font-2] gap-2 flex items-center justify-center">
+    <div className="font-[font1] gap-4 flex overflow-hidden items-center justify-center px-[5vw]">
       <Link
-        className="text-[7.5vh] leading-[6vh] border-5 pt-3 rounded-full uppercase px-10 "
+        className="text-[7.5vw] leading-[6vw] border-3 pt-3 rounded-full uppercase px-10 "
         to="/projects"
       >
         Projects
       </Link>
       <Link
-        className="text-[7.5vh] leading-[6vh]  border-5 pt-3 rounded-full uppercase px-10 "
+        className="text-[7.5vw] leading-[6vw]  border-3 pt-3 rounded-full uppercase px-10 "
         to="/agence"
       >
-        Agence
+        Agency
       </Link>
     </div>
   );
